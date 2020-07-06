79403ac0a866e15eb5602b39afaae9abc38cb8c4
Andrew Kemendo <6004400+AndrewKemendo@users.noreply.github.com>
2020-07-06 14:19:04 -0400

    prints top 5 stories URI for HN API

--- APIscraper.py
+++ APIscraper.py
@@ -2,43 +2,63 @@
 
 import json
 import requests
-import io
 
 #request JSON data from HN API
 topstories = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json')
 
-#write .txt file named topstories.txt 
+#write .txt files with top n stories
 with open('topstory01.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[0]))
+    fd.write(json.dumps(topstories.json()[0]))
 
 with open('topstory02.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[1]))
+    fd.write(json.dumps(topstories.json()[1]))
 
 with open('topstory03.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[2]))
+    fd.write(json.dumps(topstories.json()[2]))
 
 with open('topstory04.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[3]))
+    fd.write(json.dumps(topstories.json()[3]))
 
 with open('topstory05.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[4]))
-
-with open('topstory06.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[5]))
-
-with open('topstory07.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[6]))
-
-with open('topstory08.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[7]))
-
-with open('topstory09.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[8]))
-
-with open('topstory10.txt','w') as fd:
-      fd.write(json.dumps(topstories.json()[9]))
+    fd.write(json.dumps(topstories.json()[4]))
 
 #get title and URL for each of the top 30
+with open('topstory01.txt', 'r') as myfile:
+    data_0 = myfile.read()
+
+new_url_0 = f"https://hacker-news.firebaseio.com/v0/item/{data_0}.json"
+
+with open('topstory02.txt', 'r') as myfile:
+    data_1 = myfile.read()
+
+new_url_1 = f"https://hacker-news.firebaseio.com/v0/item/{data_1}.json"
+
+with open('topstory03.txt', 'r') as myfile:
+    data_2 = myfile.read()
+
+new_url_2 = f"https://hacker-news.firebaseio.com/v0/item/{data_2}.json"
+
+with open('topstory04.txt', 'r') as myfile:
+    data_3 = myfile.read()
+
+new_url_3 = f"https://hacker-news.firebaseio.com/v0/item/{data_3}.json"
+
+with open('topstory05.txt', 'r') as myfile:
+    data_4 = myfile.read()
+
+new_url_4 = f"https://hacker-news.firebaseio.com/v0/item/{data_4}.json"
+
+print(new_url_0)
+print(new_url_1)
+print(new_url_2)
+print(new_url_3)
+print(new_url_4)
+
+
+#story1 = requests.get(string_in_string)
+
+
+#urlparse
 
 
 #top1 = request.get(https://hacker-news.firebaseio.com/v0/item/{topstoryid1}.json)
--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Pulling First Page of HN every 6 hours
 Credits:
 https://github.com/HackerNews/API
 
+https://www.dataquest.io/blog/tutorial-python-scripts-command-line/
+
 https://towardsdatascience.com/getting-started-with-apis-in-python-to-gather-data-1185796b1ec3
